4359992bd45688d32bee371188c401491e30da3e
Trishnangshu Goswami <trishnangshugoswami@yahoo.in>
2022-11-29 01:08:13 +0530

    provider added redux

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import configureAppStore from './store/store';
+import { Provider } from 'react-redux';
+import { ToastContainer, toast } from 'react-toastify';
+
+const store = configureAppStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
