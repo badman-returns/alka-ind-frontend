b4cf87dbb37d7b4b089647addd44d33c723b15d0
Trishnangshu Goswami <trishnangshugoswami@yahoo.in>
2022-11-29 01:07:27 +0530

    React toastify added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { BrowserRouter, } from 'react-router-dom'
 import Router from './routes/routes';
+import { ToastContainer } from 'react-toastify';
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Router />
+        <ToastContainer position="bottom-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"/>
+          <Router />
       </BrowserRouter>
     </div>
   );
