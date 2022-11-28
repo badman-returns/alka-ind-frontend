12bb5369405ca9f55f36a93cd35adcf9de898940
Trishnangshu Goswami <trishnangshugoswami@yahoo.in>
2022-11-29 01:06:51 +0530

    about reducer, actions and async functions added

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,5 +1,6 @@
-import {combineReducers} from 'redux'
+import { combineReducers } from 'redux'
+import about from './slices/about'
 
 export const rootReducer = combineReducers({
-
+    about,
 })
---
+++ src/store/slices/about.js
@@ -0,0 +1,54 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { getAbout, postAbout } from '../../services/about.service';
+
+const initialState = {
+    about: null,
+    loading: false,
+    error: false,
+};
+
+const aboutSlice = createSlice({
+    name: 'about',
+    initialState,
+    reducers: {
+        storeAbout: (state, { payload }) => {
+            state.about = payload;
+        },
+    }
+});
+
+// Action
+export const { storeAbout } = aboutSlice.actions;
+
+// Selector
+export const aboutSelector = state => state.about || {}
+
+// Reducer
+export default aboutSlice.reducer;
+
+// Async thunk action
+export function fetchAbout() {
+    return async dispatch => {
+        try {
+            const response = await getAbout();
+            const about = response.ResponseData;
+            dispatch(storeAbout(about));
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+}
+
+export function insertAbout(payload) {
+    return async dispatch => {
+        try {
+            const response = await postAbout(payload);
+            let status = response.status;
+            let message = response.data.ResponseMessage;
+            dispatch(fetchAbout());
+            return {message, status};
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+}
