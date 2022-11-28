01c4527c288849df1f7c6a106394105962e83086
Trishnangshu Goswami <trishnangshugoswami@yahoo.in>
2022-11-29 01:05:59 +0530

    use auth hook modified

--- src/utils/useAuth.js
+++ src/utils/useAuth.js
@@ -1,51 +1,51 @@
-import React, {useContext, createContext, useMemo, useState} from "react";
-import {  useNavigate } from "react-router";
+import React, { useContext, createContext, useMemo, useState } from "react";
+import { useNavigate } from "react-router";
 import baseService from "../services/base.service";
 
 const AuthContext = createContext();
 
-export const AuthProvider = ({children}) => {
-    const [user,setUser] = useState(null);
-    const [token, setToken] = useState(null);
+export const AuthProvider = ({ children }) => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-    const login = async(email, password) => {
-        try {
-          let response = await baseService.login(email, password);
-          if(response.status === 200){
-            const token = response.headers['authorization'];
-            const user = response.data;
-            if(token && user){
-                window.localStorage.setItem('token', token);
-                window.localStorage.setItem('user', JSON.stringify(user));
-                setUser(user);
-                setToken(token);
-                navigate('/admin/dashboard')
-            }
-          }
-        } catch (error) {
-            console.log(error);
-            throw new Error(error);
+  const login = async (email, password) => {
+    try {
+      let response = await baseService.login(email, password);
+      if (response.status === 200) {
+        const token = response.headers['authorization'];
+        const user = response.data;
+        if (token && user) {
+          window.localStorage.setItem('token', token);
+          window.localStorage.setItem('user', JSON.stringify(user));
+          navigate('/admin/dashboard')
         }
+      }
+    } catch (error) {
+      console.log(error);
+      throw new Error(error);
     }
+  }
 
-    const logout = () => {
-      baseService.logout().then(() => {
-        console.log('Logout');
-        navigate('/admin/login');
-        setToken(null);
-        setUser(null);
-      }).catch((error) => {
-        console.log(error);
-      })
+  const logout = () => {
+    baseService.logout().then(() => {
+      console.log('Logout');
+      navigate('/admin/login');
+    }).catch((error) => {
+      console.log(error);
+    })
+  }
+
+  const loggedInStatus = () => {
+    const token = localStorage.getItem('token');
+    if(token && token !== null){
+      return true;
     }
+  }
 
-    const value = useMemo(() => ({user, token, login, logout}), [user]);
+  const value = { login, logout, loggedInStatus };
 
-    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
 export const useAuth = () => {
-    return useContext(AuthContext);
+  return useContext(AuthContext);
 }
