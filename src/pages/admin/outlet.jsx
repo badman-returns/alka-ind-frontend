f1533f279d6d3c80cb1391fe9e435875cd7f048d
Trishnangshu Goswami <trishnangshugoswami@yahoo.in>
2022-12-06 12:50:04 +0530

    remove unused code

--- src/pages/admin/outlet.jsx
+++ src/pages/admin/outlet.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router";
-import { useAuth } from "../../utils/useAuth";
 
 export default function AdminOutlet(){
     const navigate = useNavigate();
-    const {token, user} = useAuth();
 
     useEffect(() => {
         navigate('/admin/login')
