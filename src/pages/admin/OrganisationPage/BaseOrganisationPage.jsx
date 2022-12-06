8d4f0601b144e47194e300b207e355bb4f47d733
Trishnangshu Goswami <trishnangshugoswami@yahoo.in>
2022-12-06 12:52:10 +0530

    Base Organisation Page added

---
+++ src/pages/admin/OrganisationPage/BaseOrganisationPage.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { postOrganisationInfo } from '../../../store/slices/organisation';
+import { fetchOrganisationInfo, organisationSelector } from '../../../store/slices/organisation';
+import OrganisatonPage from './OrganisatonPage'
+
+function BaseOrganisationPage() {
+    const dispatch = useDispatch();
+    const { orgInfo } = useSelector(organisationSelector);
+
+    const fetchOrganisationData = () => {
+        dispatch(fetchOrganisationInfo());
+    }
+
+    const onFileUpload = (event) => {
+        if (event.target.files.length > 0) {
+            const logo = event.target.files[0];
+
+            let formData = new FormData();
+            formData.append('name', orgInfo.name);
+            formData.append('phone', orgInfo.phone);
+            formData.append('email', orgInfo.email);
+            formData.append('address', orgInfo.address);
+            formData.append('logo', logo);
+            console.log(formData);
+            uploadLogo(formData);
+        }
+    };
+
+    const uploadLogo = async (formData) => {
+        const response = await dispatch(postOrganisationInfo(formData));
+        toast.success(response);
+    }
+
+    useEffect(() => {
+        fetchOrganisationData();
+        // eslint-disable-next-line
+    }, []);
+
+    return orgInfo ? <OrganisatonPage organisationInfo={orgInfo} onFileUpload={onFileUpload} /> : null;
+}
+
+export default OrganisationPageWrapper
--- src/pages/admin/OrganisationPage/OrganisatonPage.jsx
+++ src/pages/admin/OrganisationPage/OrganisatonPage.jsx
@@ -1,8 +1,69 @@
+import { Box, Grid, Typography, Button } from '@mui/material'
+import { Stack } from '@mui/system';
 import React from 'react'
 
-function OrganisatonPage() {
+function OrganisatonPage({ organisationInfo, onFileUpload }) {
+  const { name, phone, email, address, fileURL } = organisationInfo;
   return (
-    <div>OrganisatonPage</div>
+    <React.Fragment>
+      <Grid container flexDirection='column' gap={2}>
+        <Grid item>
+          <Grid container flexDirection='row' justifyContent='space-between'>
+            <Box>
+              <Typography variant='h4'>Organisation</Typography>
+            </Box>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Grid container justifyContent='center'>
+            <Grid item>
+              <Box width='45rem' height='30rem' sx={{ border: '1px solid grey' }}>
+                <Grid container flexDirection='column' gap={2} alignItems='center'>
+                  {
+                    fileURL !== null ? (<Grid item sx={{ paddingTop: '4rem' }}>
+                      <Box width='10rem'>
+                        <img src={fileURL} alt='logo' width='100%' />
+                      </Box>
+                    </Grid>) :
+                      <Grid item sx={{ paddingTop: '2rem' }}>
+                        <Grid container alignItems='center' justifyContent='center' width='10rem' height='10rem' sx={{ border: '1px solid grey' }}>
+                          <Grid item>
+                            <Button variant='contained' component='label'>
+                              Add Logo
+                              <input hidden accept='image/*' type='file' onChange={onFileUpload}></input>
+                            </Button>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                  }
+                  <Grid item>
+                    <span>
+                      <Typography variant='h6'>Name: {name}</Typography>
+                      <Typography variant='h6'>Phone: {phone}</Typography>
+                      <Typography variant='h6'>Email: {email}</Typography>
+                      <Typography variant='h6'>Address: {address}</Typography>
+                    </span>
+                  </Grid>
+                  <Grid item>
+                    <Stack direction='columns' gap={2}>
+                      <Box alignItems='center'>
+                        <Button variant='contained' component='label'>
+                        <input hidden accept='image/*' type='file' onChange={onFileUpload}></input>
+                          Edit Logo
+                        </Button>
+                      </Box>
+                      <Box alignItems='center'>
+                        <Button variant='contained'>Edit Details</Button>
+                      </Box>
+                    </Stack>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </React.Fragment>
   )
 }
 
