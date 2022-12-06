afb0a87198fad696b498bac1897e9d6f98138afb
Trishnangshu Goswami <trishnangshugoswami@yahoo.in>
2022-12-06 12:53:09 +0530

    interceptor added in base service

--- src/services/base.service.js
+++ src/services/base.service.js
@@ -1,37 +1,61 @@
 import axios from "axios";
+import { dispatchLoader } from "../store/slices/loader";
+import {store} from '../index';
 
-class BaseService {
+const getAuthorizationClient = axios.create(
+  {
+    headers: {
+      Authorization: `bearer ${localStorage.getItem('token')}`
+    },
+    baseURL: process.env.REACT_APP_SERVER_BASE_URL,
+  });
 
-  getAuthorizationClient = () => axios.create(
-    {
-      headers: {
-        Authorization: `bearer ${localStorage.getItem('token')}`
+  getAuthorizationClient.interceptors.request.use((request) => {
+    store.dispatch(dispatchLoader(true));
+    return request;
+  }, (error) => {
+    return Promise.reject(error);
+  });
+
+  getAuthorizationClient.interceptors.response.use((response) => {
+    if(response.status=== 200){
+      store.dispatch(dispatchLoader(false));
+    }
+    return response;
+  }, (error) => {
+    return Promise.reject(error);
+  });
+
+
+const login = (email, password) => {
+  return axios
+    .get(`${process.env.REACT_APP_SERVER_BASE_URL}/admin/authentication`, {
+      auth: {
+        username: email,
+        password: password,
       },
-      baseURL: process.env.REACT_APP_SERVER_BASE_URL,
-    });
-
-  login = (email, password) => {
-    return axios
-      .get(`${process.env.REACT_APP_SERVER_BASE_URL}/admin/authentication`, {
-        auth: {
-          username: email,
-          password: password,
-        },
-      })
-      .then((response) => {
-        return response;
-      });
-  };
-
-  logout = () => {
-    return new Promise((resolve, reject) => {
-      if (localStorage.getItem('token') != null) {
-        localStorage.clear();
-        return resolve(true);
-      }
-      return reject(true);
     })
-  }
+    .then((response) => {
+      return response;
+    });
+};
+
+const logout = () => {
+  return new Promise((resolve, reject) => {
+    if (localStorage.getItem('token') != null) {
+      localStorage.clear();
+      return resolve(true);
+    }
+    return reject(true);
+  })
 }
 
-export default new BaseService();
+const BaseLoginService = login;
+const BaseLogoutService = logout;
+const GetAuthorizationClient = getAuthorizationClient;
+
+export {
+  BaseLoginService,
+  BaseLogoutService,
+  GetAuthorizationClient,
+};
