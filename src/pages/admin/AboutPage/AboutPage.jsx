d136be5b73f4040a864a508110d73ab4baaf36b4
Trishnangshu Goswami <trishnangshugoswami@yahoo.in>
2022-12-06 12:51:56 +0530

    Base About Page added

--- src/pages/admin/AboutPage/AboutPage.jsx
+++ src/pages/admin/AboutPage/AboutPage.jsx
@@ -1,47 +1,17 @@
 import { Button, Grid, Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import RichEditor from '../../../components/RichEditor/RichEditor';
-import { useDispatch, useSelector } from 'react-redux';
-import { aboutSelector, fetchAbout, insertAbout } from '../../../store/slices/about';
-import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-function AboutPage() {
-  const dispatch = useDispatch();
-  const { about } = useSelector(aboutSelector);
-  const { content } = about ? about : {};
-  const [richText, setRichText] = useState('');
-
-  const fetchAboutData = () => {
-    dispatch(fetchAbout());
-  }
-
-  const handleOnChange = (e) => {
-    setRichText(e);
-  }
-
-  const handleSubmit = async () => {
-    const result = await dispatch(insertAbout(richText));
-    const { message, status } = result;
-    if (status !== 200) {
-      toast.error(message);
-    } else {
-      toast.success(message);
-    }
-  }
-
-  useEffect(() => {
-    fetchAboutData();
-  }, []);
-
+function AboutPage({handleOnChange, handleSubmit, content}) {
   return (
     <React.Fragment>
       <Grid container flexDirection='column' gap={2}>
         <Grid item>
           <Grid container flexDirection='row' justifyContent='space-between'>
             <Box>
-              <Typography variant='h4'>About Page</Typography>
+              <Typography variant='h4'>About</Typography>
             </Box>
             <Box alignItems='center'>
               <Button variant='contained' onClick={handleSubmit}>Submit</Button>
---
+++ src/pages/admin/AboutPage/BaseAboutPage.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { aboutSelector, fetchAbout, insertAbout } from '../../../store/slices/about';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import AboutPage from './AboutPage';
+
+function BaseAboutPage() {
+    const dispatch = useDispatch();
+    const { content } = useSelector(aboutSelector);
+    const [richText, setRichText] = useState('');
+
+    const fetchAboutData = () => {
+        dispatch(fetchAbout());
+    }
+
+    const handleOnChange = (e) => {
+        setRichText(e);
+    }
+
+    const handleSubmit = async () => {
+        const result = await dispatch(insertAbout(richText));
+        const { message, status } = result;
+        if (status !== 200) {
+            toast.error(message);
+        } else {
+            toast.success(message);
+        }
+    }
+
+    useEffect(() => {
+        fetchAboutData();
+    }, []);
+
+    return content ? <AboutPage handleOnChange={handleOnChange} handleSubmit={handleSubmit} content={content} /> : null
+}
+
+export default AboutPageWrapper;
