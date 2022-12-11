4f64ba0ae9eee463a8adebae77b30d80c91e6241
Trishnangshu Goswami <trishnangshugoswami@yahoo.in>
2022-12-11 10:45:49 +0530

    footer container added

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Grid, Box, Stack, Typography, Container } from '@mui/material';
+
+
+function Footer() {
+    return (
+        <Box width='100%' height='50vh' sx={{ backgroundColor: '#000' }}>
+            <Container fluid>
+                
+            </Container>
+        </Box>
+    )
+}
+
+export default Footer
