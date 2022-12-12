eb915a487b58559509f2f221b9c9a8410ee5bbd8
Trishnangshu Goswami <trishnangshugoswami@yahoo.in>
2022-12-13 01:59:24 +0530

    reducers fix

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import about from './slices/about';
 import organisation from './slices/organisation';
 import loader from './slices/loader';
+import carousel from './slices/carousel';
 
 export const rootReducer = combineReducers({
     about,
     organisation,
     loader,
+    carousel,
 })
