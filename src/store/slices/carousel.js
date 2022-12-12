e9fae662cb943ca3f72209292a6164421f0e481b
Trishnangshu Goswami <trishnangshugoswami@yahoo.in>
2022-12-13 01:58:54 +0530

    carousel reducer

---
+++ src/store/slices/carousel.js
@@ -0,0 +1,55 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { getCarousels, insertCarousel } from '../../services/carousel.service';
+
+const initialState = {
+    carousels: null,
+};
+
+const carouselSlice = createSlice({
+    name: 'carousels',
+    initialState,
+    reducers: {
+        storeCarousel: (state, {payload}) => {
+            state.carousels = payload;
+        }
+    }
+});
+
+// Actions
+export const { storeCarousel } = carouselSlice.actions;
+
+// Selector
+export const carouselSelector = state => state.carousel || {};
+
+// Reducer
+export default carouselSlice.reducer;
+
+// Async thunk action
+export function fetchCarousels(){
+    return async dispatch => {
+        try {
+            const response = await getCarousels();
+            const carousels = response.ResponseData;
+            dispatch(storeCarousel(carousels));
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+}
+
+
+export function addCarousel(payload){
+    return async dispatch => {
+        try {
+            const response = await insertCarousel(payload);
+            if(response.status === 200){
+                dispatch(fetchCarousels());
+                return response.data.ResponseMessage;
+            } else {
+                return response;
+            }
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+}
