8cfd6198bea537475f4825d8e49316844eda3f87
Sushanta Bhowmick <bhosushanta922@gmail.com>
2023-01-17 18:29:07 +0530

    centralized the Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,25 @@
 import React from "react";
-import { Box, Stack, Typography } from '@mui/material';
+import { Box,Grid, Stack, Typography } from '@mui/material';
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 import MailIcon from '@mui/icons-material/Mail';
 
 function Header({orgInfo}) {
     return (
-        <Box sx={{ backgroundColor: '#000' }} width='100%' height='5vh'>
-            <Stack direction='row' justifyContent='flex-end' gap={2} marginRight='1rem' padding='3px' color='#fff'>
-                <Typography variant='subtitle1'><b>Reach us out here:  </b></Typography>
+        <Box sx={{ backgroundColor: '#000' }} display='flex' alignItems={'center'}  justifyContent={'flex-end'} width='100%' height='5vh' >
+        
+            <Stack display={'flex'} direction='row' gap={2} marginRight='1rem' color='#fff' >
+                <Typography  variant='subtitle1'><b>Reach us out here:  </b></Typography>
                 <Stack direction='row' gap={1}>
                     <LocalPhoneIcon />
-                    <Typography variant="subtitle1"><b>{orgInfo.phone}</b></Typography>
+                    <Typography variant="subtitle1"><b>{orgInfo.phone}</b></Typography>     
                 </Stack>
+
                 <Stack direction='row' gap={1}>
                     <MailIcon />
                     <Typography variant="subtitle1"><b>{orgInfo.email}</b></Typography>
                 </Stack>
             </Stack>
+    
         </Box>
     )
 }
