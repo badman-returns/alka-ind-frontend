2b6ce5c7002cd3c7f7bf3744c3bf0d1e462da11b
Sushanta Bhowmick <bhosushanta922@gmail.com>
2023-01-17 19:25:17 +0530

    fixed the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box,Grid, Stack, Typography } from '@mui/material';
+import { Box,Stack, Typography } from '@mui/material';
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 import MailIcon from '@mui/icons-material/Mail';
 
